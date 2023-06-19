899ab185245395b7c584625e7a01bf1ef6b764fc
andrewcargill <andycargill01@aol.com>
2023-06-19 21:06:50 +0200

    testing pages folder

--- mill/src/About.jsx
+++ mill/src/About.jsx
@@ -1,7 +1,13 @@
-import React from 'react'  
-class About extends React.Component {  
-  render() {  
-    return <h1>About page on index</h1>  
-  }  
-}  
-export default About 
+import React from 'react';
+import Contact from './pages/Contact';
+
+const About = () => {
+  return (
+    <div>
+      <h1>Hello, React!</h1>
+      <Contact />
+    </div>
+  );
+};
+
+export default About;
--- mill/src/Contact.jsx
+++
@@ -1,7 +0,0 @@
-import React from 'react'  
-class Contact extends React.Component {  
-  render() {  
-    return <h1>Contact page on about</h1>  
-  }  
-}  
-export default Contact 
