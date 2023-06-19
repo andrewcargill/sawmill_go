b71c5ca20f50bdcc7338b0d34ca680f251d2dc7e
andrewcargill <andycargill01@aol.com>
2023-06-19 20:00:44 +0200

    testing about.jsx routing and path

--- mill/src/Home.jsx
+++ mill/src/Home.jsx
@@ -1,10 +1,13 @@
 import React from 'react';
+import About from './About';
 
 
 const Home = () => {
   return (
     <div>
       <h1>Home Content</h1>
+      <h3>Imported About Content below</h3>
+      <About />
     </div>
   );
 };
