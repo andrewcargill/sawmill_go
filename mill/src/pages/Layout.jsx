d242e7c7a7936451dddd5ac5fe97865fe5c5cb69
andrewcargill <andycargill01@aol.com>
2023-06-20 15:15:13 +0200

    test bootstrap with nav bar

--- mill/src/pages/Layout.jsx
+++ mill/src/pages/Layout.jsx
@@ -3,25 +3,38 @@ import { Outlet, Link } from "react-router-dom";
 const Layout = () => {
   return (
     <>
-      <nav>
-        <ul>
-          <li>
-            <Link to="">Home</Link>
-          </li>
-          <li>
-            <Link to="about">About</Link>
-          </li>
-          <li>
-            <Link to="test">Test</Link>
-          </li>
-        </ul>
+      <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+        <div className="container">
+          <Link className="navbar-brand" to="/">Home</Link>
+          <button
+            className="navbar-toggler"
+            type="button"
+            data-bs-toggle="collapse"
+            data-bs-target="#navbarNav"
+            aria-controls="navbarNav"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
+            <span className="navbar-toggler-icon"></span>
+          </button>
+          <div className="collapse navbar-collapse" id="navbarNav">
+            <ul className="navbar-nav">
+              <li className="nav-item">
+                <Link className="nav-link" to="about">About</Link>
+              </li>
+              <li className="nav-item">
+                <Link className="nav-link" to="test">Test</Link>
+              </li>
+            </ul>
+          </div>
+        </div>
       </nav>
 
-      <div className="content">
+      <div className="container mt-4">
         <Outlet /> {/* Render the nested components */}
       </div>
     </>
-  )
+  );
 };
 
 export default Layout;
