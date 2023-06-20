69154272c54b1a6b82b9026f0555d991198fe55d
andrewcargill <andycargill01@aol.com>
2023-06-20 14:48:46 +0200

    testing about routing for deployment

---
+++ mill/src/About.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import Contact from './pages/TestApi';
+
+const About = () => {
+  return (
+    <div>
+      <h1>This is the About Page!</h1>
+      <Contact />
+    </div>
+  );
+};
+
+export default About;
--- mill/src/App.js
+++ mill/src/App.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Home from './Home';
+import About from './About';
 
 const App = () => {
   return (
     <Router basename={`/${process.env.PUBLIC_URL}`}>
       <Routes>
         <Route path="" element={<Home />} />
+        <Route path="about" element={<About />} />
       </Routes>
     </Router>
   );
