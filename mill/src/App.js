c480c0d13946f6750ef5d36cd750a2af22a312f5
andrewcargill <andycargill01@aol.com>
2023-06-20 14:43:49 +0200

    testing routing

--- mill/src/App.js
+++ mill/src/App.js
@@ -1,15 +1,14 @@
 import React from 'react';
-import { Route, Routes } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Home from './Home';
-
 
 const App = () => {
   return (
-    
+    <Router basename={`/${process.env.PUBLIC_URL}`}>
       <Routes>
-        <Route path="/" element={<Home />} />
+        <Route path="" element={<Home />} />
       </Routes>
-   
+    </Router>
   );
 };
 
--- mill/src/index.js
+++ mill/src/index.js
@@ -8,9 +8,9 @@ import reportWebVitals from './reportWebVitals';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <BrowserRouter basename='/sawmill_go'>
+
     <App />
-    </BrowserRouter>
+  
   </React.StrictMode>
 );
 
