8ec792a786e7990258646828942becbd27c8adf0
andrewcargill <110658895+andrewcargill@users.noreply.github.com>
2023-08-02 18:03:09 +0200

    style login component

--- mill/src/pages/TestLogIn.jsx
+++ mill/src/pages/TestLogIn.jsx
@@ -1,71 +1,77 @@
 import React, { useEffect, useState } from 'react';
 import axios from 'axios';
 import { useNavigate } from 'react-router-dom';
+import CustomFormHeading from '../components/CustomForm/CustomFormHeading';
+import FormBoxMain from '../components/CustomForm/FormBoxMain';
+import { Button, Grid } from '@mui/material';
+import CustomInput from '../components/CustomForm/CustomInput';
 
 
 const Login = () => {
   const [username, setUsername] = useState('');
   const [password, setPassword] = useState('');
   // Create the submit method.
- 
+
 
   const submit = async e => {
-       e.preventDefault();
-       const user = {
-             username: username,
-             password: password
-            };
-       // Create the POST requuest
-       const {data} = await axios.post('https://sawmill-live-api-ecf54c3f35e6.herokuapp.com/token/',
-        user ,
-        {
-          headers: {
-            'Content-Type': 'application/json'
-          },
-           withCredentials: true
-          }
-        );
-        console.log('Data received:', data); // Log the data received from the API
+    e.preventDefault();
+    const user = {
+      username: username,
+      password: password
+    };
+    // Create the POST requuest
+    const { data } = await axios.post('https://sawmill-live-api-ecf54c3f35e6.herokuapp.com/token/',
+      user,
+      {
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        withCredentials: true
+      }
+    );
+    console.log('Data received:', data); // Log the data received from the API
 
-      // Initialize the access & refresh token in localstorage.      
-      localStorage.clear();
-      localStorage.setItem('access_token', data.access);
-      localStorage.setItem('refresh_token', data.refresh);
-      axios.defaults.headers.common['Authorization'] = `Bearer ${data['access']}`;
+    // Initialize the access & refresh token in localstorage.      
+    localStorage.clear();
+    localStorage.setItem('access_token', data.access);
+    localStorage.setItem('refresh_token', data.refresh);
+    axios.defaults.headers.common['Authorization'] = `Bearer ${data['access']}`;
 
-      window.location.href = 'home_secure';
- }
- return(
-   <div className="page">
-     <form className="Auth-form" onSubmit={submit}>
-       <div className="Auth-form-content">
-         <h3 className="Auth-form-title">Sign In</h3>
-         <div className="form-group mt-3">
-           <label>Username</label>
-           <input className="form-control mt-1" 
-             placeholder="Enter Username" 
-             name='username'  
-             type='text' value={username}
-             required 
-             onChange={e => setUsername(e.target.value)}/>
-         </div>
-         <div className="form-group mt-3">
-           <label>Password</label>
-           <input name='password' 
-             type="password"     
-             className="form-control mt-1"
-             placeholder="Enter password"
-             value={password}
-             required
-             onChange={e => setPassword(e.target.value)}/>
-         </div>
-         <div className="d-grid gap-2 mt-3">
-           <button type="submit" 
-              className="btn btn-primary">Submit</button>
-         </div>
-       </div>
-    </form>
-  </div>
+    window.location.href = 'home_secure';
+  }
+  return (
+
+
+    <div>
+      <CustomFormHeading title="Log In" />
+      <FormBoxMain>
+        <form onSubmit={submit}>
+          <Grid item xs={12}>
+            <CustomInput
+              label="Username"
+              type="text"
+              value={username}
+              onChange={e => setUsername(e.target.value)}
+              required
+            />
+          </Grid>
+          <Grid item xs={12}>
+            <CustomInput
+              label="Password"
+              type="password"
+              value={password}
+              onChange={e => setPassword(e.target.value)}
+              required
+            />
+          </Grid>
+          <Grid item xs={12} sx={{ mt: 2 }}>
+            <Button variant="contained" color="primary" fullWidth type="submit">
+              Submit
+            </Button>
+          </Grid>
+        </form>
+      </FormBoxMain>
+    </div>
   )
 }
 
