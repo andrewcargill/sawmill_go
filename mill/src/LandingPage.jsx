bd4e159edae0d6657165364680feb611064d9a32
andrewcargill <110658895+andrewcargill@users.noreply.github.com>
2023-08-02 16:56:35 +0200

    rewording of landing page

--- mill/src/LandingPage.jsx
+++ mill/src/LandingPage.jsx
@@ -17,91 +17,103 @@ const LandingPage = () => {
     const navigate = useNavigate();
 
     const handleButtonClick = () => {
-      navigate('/about');
+        navigate('/about');
     };
 
-  return (
-    <PageContentContainer>
-      <FullWidthImageContainer imageUrl={forest} />
+    return (
+        <PageContentContainer>
+            <FullWidthImageContainer imageUrl={forest} />
 
-      <CustomBox variant="primary">
-        <Grid container spacing={1}>
-          <Grid item xs={12}>
-            <CustomTypography.heading>
-              Welcome to Sawmill go!
-            </CustomTypography.heading>
-          </Grid>
-          <Grid item xs={12}>
-            <CustomTypography.subheading>
-              Empower Your Artisanal Sawmill with Sawmill Go - The Ultimate Database Solution!
-            </CustomTypography.subheading>
-          </Grid>
-          <Grid item xs={12}>
-            <CustomTypography.paragraph>
-              Seamlessly manage your inventory operations with ease. From start to finish, experience smooth and hassle-free management, allowing artisans to effortlessly oversee their operations.
-            </CustomTypography.paragraph>
-          </Grid>
-          <Grid item xs={12}>
-            <CustomButton onClick={handleButtonClick} variant="contained">
-              Read More
-            </CustomButton>
-          </Grid>
-        </Grid>
-      </CustomBox>
-      <FullWidthImageContainer imageUrl={forestTwo} />
-      <CustomBox variant="secondary">
-        <Grid container spacing={1}>
-          <Grid item xs={12}>
-            <CustomTypography.heading>
-              100% TRANSPARENT FORESTRY
-            </CustomTypography.heading>
-          </Grid>
-          <Grid item xs={12}>
-            <CustomTypography.subheading>
+            <CustomBox variant="primary">
+                <Grid container spacing={1}>
+                    <Grid item xs={12}>
+                        <CustomTypography.heading>
+                            Welcome to Sawmill go!
+                        </CustomTypography.heading>
+                    </Grid>
+                    <Grid item xs={12}>
+                        <CustomTypography.subheading>
+                            {/* Empower Your Artisanal Sawmill with Sawmill Go - The Ultimate Database Solution! */}
+                            My personal API and Frontend project for Artisan Sawmill Owners!
+                        </CustomTypography.subheading>
+                    </Grid>
+                    <Grid item xs={12}>
+                        <CustomTypography.paragraph>
+                            {/* Seamlessly manage your inventory operations with ease. From start to finish, experience smooth and hassle-free management, allowing artisans to effortlessly oversee their operations. */}
+                            I own a sawmill and forests in the North of Sweden and wanted to create a inventory management system.
+                        </CustomTypography.paragraph>
+                    </Grid>
+                    <Grid item xs={12}>
+                        <CustomButton onClick={handleButtonClick} variant="contained">
+                            GitHub ReadMe
+                        </CustomButton>
+                    </Grid>
+                </Grid>
+            </CustomBox>
 
-              Introducing "Transparent Forestry" - Revolutionizing the Industry!
-            </CustomTypography.subheading>
-          </Grid>
-          <Grid item xs={12}>
-            <CustomTypography.paragraph>
-              Discover complete information about sourced trees, from felling date to responsible individuals and precise locations. Experience unparalleled transparency and traceability for a truly distinctive offering.
-            </CustomTypography.paragraph>
-          </Grid>
-          <Grid item xs={12}>
-          <CustomButton onClick={handleButtonClick} variant="contained">
-              Read More
-            </CustomButton>
-          </Grid>
-        </Grid>
-      </CustomBox>
-      <FullWidthImageContainer imageUrl={cloud} />
-      <CustomBox variant="dark">
-        <Grid container spacing={1}>
-          <Grid item xs={12}>
-            <CustomTypography.heading>
-              UNLEASHING CLOUD COMPUTING'S POWER
-            </CustomTypography.heading>
-          </Grid>
-          <Grid item xs={12}>
-            <CustomTypography.subheading>
-              Effortlessly search your inventory with custom-built filters for seamless organization.
-            </CustomTypography.subheading>
-          </Grid>
-          <Grid item xs={12}>
-            <CustomTypography.paragraph>
-              Input tree details for precise calculations. Add GPS locations, images, notes, moisture checks, from any device.
-            </CustomTypography.paragraph>
-          </Grid>
-          <Grid item xs={12}>
-          <CustomButton onClick={handleButtonClick} variant="contained">
-              Read More
-            </CustomButton>
-          </Grid>
-        </Grid>
-      </CustomBox>
-    </PageContentContainer>
+            <FullWidthImageContainer imageUrl={cloud} />
+            <CustomBox variant="secondary">
+                <Grid container spacing={1}>
+                    <Grid item xs={12}>
+                        <CustomTypography.heading>
+                            UNLEASHING CLOUD COMPUTING'S POWER
+                        </CustomTypography.heading>
+                    </Grid>
+                    <Grid item xs={12}>
+                        <CustomTypography.subheading>
+                            Effortlessly search your inventory with custom-built filters for seamless organization.
+                        </CustomTypography.subheading>
+                    </Grid>
+                    <Grid item xs={12}>
+                        <CustomTypography.paragraph>
+                            The backend is built with MySQL Database with a Django (Python) API, hosted with Heroku. The Frontend is ReactJS using Matrial UI for styling, currently deployed via Github Pages.
+                        </CustomTypography.paragraph>
+                    </Grid>
+                    <Grid container xs={12}>
+                        <Grid item xs={6}>
+                            <CustomButton onClick={handleButtonClick} variant="contained">
+                                FrontEnd ReadMe
+                            </CustomButton>
+                        </Grid>
+                        <Grid item xs={6}>
+                            <CustomButton onClick={handleButtonClick} variant="contained">
+                                Backend Repository
+                            </CustomButton>
+                        </Grid>
 
-  );
+                    </Grid>
+
+                </Grid>
+            </CustomBox>
+            <FullWidthImageContainer imageUrl={forestTwo} />
+            <CustomBox variant="dark">
+                <Grid container spacing={1}>
+                    <Grid item xs={12}>
+                        <CustomTypography.heading>
+                            100% TRANSPARENT FORESTRY
+                        </CustomTypography.heading>
+                    </Grid>
+                    <Grid item xs={12}>
+                        <CustomTypography.subheading>
+
+                            Introducing "Transparent Forestry" - Revolutionizing the Industry!
+                        </CustomTypography.subheading>
+                    </Grid>
+                    <Grid item xs={12}>
+                        <CustomTypography.paragraph>
+                            Discover complete information about sourced trees, from felling date to responsible individuals and precise locations. Experience unparalleled transparency and traceability for a truly distinctive offering.
+                        </CustomTypography.paragraph>
+                    </Grid>
+                    <Grid item xs={12}>
+                        <CustomButton onClick={handleButtonClick} variant="contained">
+                            Read More
+                        </CustomButton>
+                    </Grid>
+                </Grid>
+            </CustomBox>
+        </PageContentContainer>
+
+    );
 };
 
 export default LandingPage;
