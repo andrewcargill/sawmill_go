a9d7e19356da1696fd3ba3f90e855cecd8ee024c
andrewcargill <110658895+andrewcargill@users.noreply.github.com>
2024-03-23 09:06:00 +0100

    link to plank detail from plank list

--- mill/src/pages/ListViews/Plank/PlankListResultsContent.jsx
+++ mill/src/pages/ListViews/Plank/PlankListResultsContent.jsx
@@ -16,93 +16,27 @@ import {
   Avatar,
   Tooltip,
 } from "@mui/material";
-import StarIcon from "@mui/icons-material/Star";
-import ArticleIcon from "@mui/icons-material/Article";
-import KeyboardArrowDownIcon from "@mui/icons-material/KeyboardArrowDown";
-import KeyboardArrowUpIcon from "@mui/icons-material/KeyboardArrowUp";
-import PersonIcon from "@mui/icons-material/Person";
-import CalendarMonthIcon from "@mui/icons-material/CalendarMonth";
-import EditIcon from "@mui/icons-material/Edit";
-import WaterIcon from "@mui/icons-material/Water";
 import { useNavigate } from "react-router";
-import { faBlackboard } from "@fortawesome/free-solid-svg-icons";
-import customTheme from "../../../customTheme";
 import CustomTypography from "../../../components/Typography/CustomTypography";
 
-const ExpandableCardContainer = styled(Card)(({ theme }) => ({
-  marginBottom: theme.spacing(2),
-  cursor: "pointer",
-  backgroundColor: theme.palette.white.main,
-  border: "black 1px solid",
-  borderRadius: "10px",
-
-  "@media (max-width: 600px)": {
-    marginBottom: theme.spacing(1),
-  },
-}));
-
-const ExpandableCardContent = styled(CardContent)(({ theme }) => ({
-  display: "flex",
-  flexDirection: "column",
-  backgroundColor: theme.palette.white.main,
-}));
 
 const PlankListResultsContent = ({ data }) => {
-  const [expanded, setExpanded] = useState(false);
-  const [openImageDialog, setOpenImageDialog] = useState(false);
-  const [selectedImage, setSelectedImage] = useState("");
-  const [imageLoaded, setImageLoaded] = useState(false);
-  const [image2Loaded, setImage2Loaded] = useState(false);
 
-  /* Handles card expanding */
-  const handleCardClick = () => {
-    setExpanded(!expanded);
-  };
-
-  /* Handles image preview */
-  const handleImageClick = (image) => {
-    setSelectedImage(image);
-    setOpenImageDialog(true);
-  };
-
-  const handleCloseImageDialog = () => {
-    setOpenImageDialog(false);
-  };
+  const navigate = useNavigate();
 
   const wholeLength = Math.floor(data.log.length);
   const wholeDepth = Math.floor(data.depth);
   const wholeWidth = Math.floor(data.width);
 
-  /* Card Click */
-  const navigate = useNavigate();
-
-  const handleEditClick = () => {
-    navigate(`/plank/${data.id}/edit`);
-  };
-
-  const handleReportClick = () => {
-    navigate(`/report/${data.id}`);
-  };
-
-  /* Image loading */
-  const handleImageLoad = () => {
-    setImageLoaded(true);
-  };
-
-  const handleImage2Load = () => {
-    setImage2Loaded(true);
-  };
-
   const calculateWidthPercentage = (width) => {
     const minWidth = 2;
     const maxWidth = 25;
-    const minPercentage = 30; // Minimum width percentage
-    const maxPercentage = 180; // Maximum width percentage
+    const minPercentage = 30;
+    const maxPercentage = 180; 
 
     if (width <= minWidth) return `${minPercentage}px`;
     if (width >= maxWidth) return `${maxPercentage}px`;
 
-    // Calculate percentage for values between minWidth and maxWidth
     const percentage =
       minPercentage +
       ((width - minWidth) / (maxWidth - minWidth)) *
@@ -113,13 +47,12 @@ const PlankListResultsContent = ({ data }) => {
   const calculateHeight = (depth) => {
     const minDepth = 2;
     const maxDepth = 25;
-    const minHeight = 30; // Minimum height in pixels
-    const maxHeight = 180; // Maximum height in pixels
+    const minHeight = 30; 
+    const maxHeight = 180; 
 
     if (depth <= minDepth) return `${minHeight}px`;
     if (depth >= maxDepth) return `${maxHeight}px`;
 
-    // Calculate height for values between minDepth and maxDepth
     const height =
       minHeight +
       ((depth - minDepth) / (maxDepth - minDepth)) * (maxHeight - minHeight);
@@ -133,6 +66,11 @@ const PlankListResultsContent = ({ data }) => {
       </Typography>
     </>
   );
+
+    const handlePlankClick = () => {
+    navigate(`/plank/${data.id}`);
+    };
+
 
   return (
     <Tooltip title={toolTipContent} arrow>
@@ -149,7 +87,7 @@ const PlankListResultsContent = ({ data }) => {
           height: calculateHeight(data.depth),
           cursor: "pointer",
         }}
-        onClick={handleCardClick}
+        onClick={handlePlankClick}
       >
         <CustomTypography.subReportHeading>
           {data.log.tree.species} {data.id}
