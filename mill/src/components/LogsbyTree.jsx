9db60740080c279d91e63695f6dd627a5122e706
andrewcargill <110658895+andrewcargill@users.noreply.github.com>
2024-03-23 11:53:14 +0100

    styles plank detail view

--- mill/src/components/LogsbyTree.jsx
+++ mill/src/components/LogsbyTree.jsx
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from "react";
 import axios from "axios";
 import { Link, useNavigate } from "react-router-dom";
-import { Button, Grid, Typography } from "@mui/material";
+import { Button, Grid, Tooltip, Typography } from "@mui/material";
 import CustomTypography from "./Typography/CustomTypography";
 
 const LogsByTree = ({ treeId }) => {
@@ -43,38 +43,47 @@ const LogsByTree = ({ treeId }) => {
     <Grid container spacing={2} m={1}>
       {logs && logs.length > 0 ? (
         logs.map((log) => (
+          <Tooltip arrow title={` Milled:
+          
+           ${log.date.substring(2)}
+         `} key={log.id}>
           <Grid item xs={6} sm={3} lg={2} key={log.id}>
             <div
-              style={{
-                backgroundColor: "lightgrey",
-                padding: "2%",
-                borderRadius: "5px",
-                display: "flex",
-                flexDirection: "column",
-                alignItems: "center",
-                cursor: "pointer",
-                maxWidth: "200px",
-                maxHeight: "100px",
-                minWidth: "150px",
-              }}
+                  style={{
+                    border: "2px solid green",
+                    borderRadius: "50%", // This ensures the shape is always a circle
+                    width: "100px",
+                    maxWidth: "100px",
+                    maxHeight: "100px",
+                    height: "100px",
+                    display: "flex",
+                    flexDirection: "column",
+                    justifyContent: "center",
+                    alignItems: "center",
+                    cursor: "pointer",
+                    boxSizing: "border-box", // Ensures padding is included in the width/height calculations
+                  }}
               onClick={handleLogClick.bind(this, log.id)}
             >
               {" "}
               {/* Added some styling for vertical alignment */}
               <CustomTypography.subReportHeading>
-                Log ID: {log.id}
+                ID: {log.id}
               </CustomTypography.subReportHeading>
             
-            
+           
               <CustomTypography.subReportContent>
-                Date Cut: {log.date.substring(2)}
+                L={Math.floor(log.length)}
+          
               </CustomTypography.subReportContent>
               <CustomTypography.subReportContent>
-                              Length: {log.length}
+              D={Math.floor(log.diameter)}
               </CustomTypography.subReportContent>
             </div>
           </Grid>
+          </Tooltip>
         ))
+
       ) : (
         <Typography>No logs found for Tree ID: {treeId}</Typography>
       )}
--- mill/src/pages/MillPlankDetail.jsx
+++ mill/src/pages/MillPlankDetail.jsx
@@ -7,14 +7,18 @@ import { Container, Row, Col, Table, Button } from "react-bootstrap";
 import { Link } from "react-router-dom";
 import LogsByTree from "../components/LogsbyTree";
 import WaterByPlank from "../components/WaterByPlank";
+import DoneIcon from '@mui/icons-material/Done';
+import CloseIcon from '@mui/icons-material/Close';
 
 import {
   faLeftLong,
   faPenToSquare,
-  faFile
-
+  faFile,
 } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import PageContentContainer from "../components/CustomBoxes/PageContentContainer";
+import { Chip, Dialog, DialogContent, Grid } from "@mui/material";
+import CustomHeaderWithNavEdit from "../components/CustomFormHeaders/CustomHeaderWithNavEdit";
 
 const PlankDetail = () => {
   const { id } = useParams();
@@ -22,6 +26,8 @@ const PlankDetail = () => {
   const [length, setLength] = useState(null);
   const [treeId, setTreeId] = useState(null);
   const [species, setSpecies] = useState(null);
+  const [openDialog, setOpenDialog] = useState(false);
+  const [openImage2, setOpenImage2] = useState(false);
 
   const navigate = useNavigate();
 
@@ -53,169 +59,254 @@ const PlankDetail = () => {
   }
 
   const getLiveEdgeStatus = (live_edge) => {
-    return live_edge ? "Yes" : "No";
+    return live_edge ? <DoneIcon /> : <CloseIcon />;
   };
 
   const getFurnitureStatus = (furniture) => {
-    return furniture ? "Yes" : "No";
+    return furniture ? <DoneIcon /> : <CloseIcon />;
   };
 
   const getStructuralStatus = (structural) => {
-    return structural ? "Yes" : "No";
+    return structural ? <DoneIcon /> : <CloseIcon />;
   };
 
   const getGeneralStatus = (general) => {
-    return general ? "Yes" : "No";
+    return general ? <DoneIcon /> : <CloseIcon />;
   };
 
   const handleGoBack = () => {
     navigate(-1);
   };
 
+  const handleEditClick = () => {
+    navigate(`/plank/${id}/edit`);
+  };
+
+  const handleLogClick = () => {
+    navigate(`/log/${plank.log.id}`);
+  };
+
+  const handleTreeClick = () => {
+    navigate(`/tree/${plank.log.tree.id}`);
+  };
+
+  //Image Dialog
+
+ const handleOpenDialog = () => setOpenDialog(true);
+  const handleCloseDialog = () => setOpenDialog(false);
+ const handleOpenImage2 = () => setOpenImage2(true);
+  const handleCloseImage2 = () => setOpenImage2(false);
+
   return (
-    <div className="page">
-      <Container className="pb-4 small">
-        <Row className="pb-4 mill-buttons">
-          <Col xs={6}>
-            <h2>Plank {id} Info</h2>
-          </Col>
-          <Col xs={2}>
-            <Button small onClick={handleGoBack}>
-              <FontAwesomeIcon icon={faLeftLong} />
+    <PageContentContainer>
+      <Grid Container>
+        <CustomHeaderWithNavEdit
+          title={`Plank ${id} Info`}
+          handleGoBack={handleGoBack}
+          handleEditClick={handleEditClick}
+        />
+        <Grid item container xs={12}>
+          <Link to={`/report/${plank.id}/`}>
+            <Button small>
+              <FontAwesomeIcon icon={faFile} />
             </Button>
-          </Col>
-          <Col xs={2}>
-            <Link to={`/plank/${plank.id}/edit`}>
-              <Button small >
-                <FontAwesomeIcon icon={faPenToSquare} />
-              </Button>
-            </Link>
-          </Col>
-          <Col xs={2}>
-            <Link to={`/report/${plank.id}/`}>
-              <Button small >
-                <FontAwesomeIcon icon={faFile} />
-              </Button>
-            </Link>
-          </Col>
-        </Row>
-        <Row>
-          <Col>
-            <Table bordered>
-              <tbody>
-                <tr>
-                  <th>Date:</th>
-                  <td>{plank.date}</td>
-                </tr>
-                <tr>
-                  <th>Width:</th>
-                  <td>{plank.width}</td>
-                </tr>
-                <tr>
-                  <th>Depth:</th>
-                  <td>{plank.depth}</td>
-                </tr>
-                <tr>
-                  <th>Length:</th>
-                  <td>{length}</td>
-                </tr>
-                <tr>
-                  <th>Species:</th>
-                  <td>{species}</td>
-                </tr>
-                <tr>
-                  <th>Grade:</th>
-                  <td>{plank.wood_grade}</td>
-                </tr>
-                <tr>
-                  <th>Log ID:</th>
+          </Link>
+        </Grid>
+        <Grid item container xs={12}>
+          
+              <Table bordered>
+                <tbody>
 
-                  <td>
-                  <Link to={`/log/${plank.log.id}`}>
-              <Button>{plank.log.id}</Button>
-                  </Link>
-                    
-                    </td>
-                </tr>
-                <tr>
-                  <th>Tree ID:</th>
-  
-                  <td>
-                  <Link to={`/tree/${plank.log.tree.id}`}>
-              <Button>{plank.log.tree.id}</Button>
-                  </Link>
-                    
-                    </td>
-                </tr>
+                  <tr>
+                    <th>Date:</th>
+                    <td>{plank.date}</td>
+                  </tr>
+                  <tr>
+                    <th>Width:</th>
+                    <td>{plank.width}</td>
+                  </tr>
+                  <tr>
+                    <th>Depth:</th>
+                    <td>{plank.depth}</td>
+                  </tr>
+                  <tr>
+                    <th>Length:</th>
+                    <td>{length}</td>
+                  </tr>
+                  <tr>
+                    <th>Species:</th>
+                    <td>{species}</td>
+                  </tr>
+                  <tr>
+                    <th>Grade:</th>
+                    <td>{plank.wood_grade}</td>
+                  </tr>
+                  <tr>
+                    <th>Log ID:</th>
 
-                <tr>
-                  <th>Operator:</th>
-                  <td>{plank.operator}</td>
-                </tr>
-              </tbody>
-              <tbody>
-                <tr>
-                  <td colSpan={2}>
-                    <strong>Operator's notes:</strong>
-                    <p>{plank.info}</p>
-                  </td>
-                </tr>
-                <tr>
-                  <td colSpan={2}>
-                    <strong>Categories:</strong>
-                    <Row>
-                      <Col>
-                        <p>Live-Edge: {getLiveEdgeStatus(plank?.live_edge)}</p>
-                        <p>Furniture: {getFurnitureStatus(plank?.furniture)}</p>
-                      </Col>
-                      <Col>
-                        <p>
-                          Structural: {getStructuralStatus(plank?.structural)}
-                        </p>
-                        <p>General: {getGeneralStatus(plank?.general)}</p>
-                      </Col>
-                    </Row>
-                  </td>
-                </tr>
-                <tr>
-                  <td colSpan={2}>
-                  Image 1
+                    <td>
+                      <Chip
+                        label={plank.log.id}
+                        onClick={handleLogClick}
+                       
+                        color="dark"
+                      />
+                    </td>
+                  </tr>
+                  <tr>
+                    <th>Tree ID:</th>
+
+                    <td>
+                      <Chip
+                        label={plank.log.tree.id}
+                        onClick={handleTreeClick}
+                    
+                        color="dark"
+                      />
+
+                    </td>
+                  </tr>
+               
+
+                  <tr>
+                    <th>Operator:</th>
+                    <td>{plank.operator}</td>
+                  </tr>
+                </tbody>
+              
+                </Table>
+                <Table bordered>
+                <tbody>
+                  <tr>
+                    <td colSpan={2}>
+                      <strong>Operator's notes:</strong>
+                      <p>{plank.info}</p>
+                    </td>
+                  </tr>
+                 </tbody>
+              </Table>
+              <Table bordered>
+                <tbody>
+                  <tr>
+                    <td colSpan={2}>
+                      <strong>Categories:</strong>
+                      
+                      <Grid container xs={12} p={2} >
+                
+                        <Grid item container lg={3}  sm={3} xs={6} >
+                          <p>
+                            Live-Edge: {getLiveEdgeStatus(plank?.live_edge)}
+                          </p>
+                          </Grid>
+                          <Grid item container  lg={3} sm={3} xs={6}>
+                          <p>
+                            Furniture: {getFurnitureStatus(plank?.furniture)}
+                          </p>
+                          </Grid>
+                          <Grid item container  lg={3}  sm={3} xs={6}>
+                        
+                          <p>
+                            Structural: {getStructuralStatus(plank?.structural)}
+                          </p>
+                          </Grid>
+                          <Grid item container  lg={3}  sm={3} xs={6}>
+                          <p>General: {getGeneralStatus(plank?.general)}</p>
+                          </Grid>
+                        
+                      
+                      </Grid>
+                    </td>
+                  </tr>
+                  <Grid item container xs={12}>
+                    <Grid item container xs={6}
+                     onClick={plank.image1 ? handleOpenDialog : undefined} // Only set onClick handler if tree.image exists
+                     style={{ cursor: plank.image1 ? "pointer" : "default" }} // Change cursor style based on tree.image existence
+                    >
+             
+                    <td colSpan={2}>
+                      Image 1
+                      {plank.image1 && (
+                        <img
+                          src={plank.image1}
+                          alt="Tree Image"
+                          style={{ maxWidth: "100%", height: "auto" }}
+                        />
+                      )}
+                    </td>
+               
+                  </Grid>
+                  <Grid item container xs={6}
+                  onClick={plank.image2 ? handleOpenImage2 : undefined} // Only set onClick handler if tree.image exists
+                  style={{ cursor: plank.image2 ? "pointer" : "default" }} // Change cursor style based on tree.image existence
+                 >
+                 
+                    <td colSpan={2}>
+                      Image 2
+                      {plank.image2 && (
+                        <img
+                          src={plank.image2}
+                          alt="Tree Image"
+                          style={{ maxWidth: "100%", height: "auto" }}
+                        />
+                      )}
+                    </td>
+               
+                  </Grid>
+                  </Grid>
+                  <tr>
+                    <td colSpan={2}>
+                      <strong>Moisture Checks:</strong>
+
+                      <div>
+                        <WaterByPlank plankId={id} />
+                      </div>
+                    </td>
+                  </tr>
+                </tbody>
+              </Table>
+              <Dialog
+                open={openDialog}
+                onClose={handleCloseDialog}
+                maxWidth="lg" // Adjust the maximum size of the dialog if needed
+                aria-labelledby="image-dialog-title"
+              >
+                <DialogContent
+                  onClick={handleCloseDialog}
+                  style={{ cursor: "pointer" }}
+                >
                   {plank.image1 && (
                     <img
                       src={plank.image1}
-                      alt="Tree Image"
-                      style={{ maxWidth: "100%", height: "auto" }}
+                      alt="Plank Image"
+                      style={{ maxWidth: "80vh", height: "auto" }}
                     />
                   )}
-                  </td>
-                </tr>
-                <tr>
-                  <td colSpan={2}>
-                  Image 2
-                  {plank.image2 && (
+                </DialogContent>
+              </Dialog>
+
+              <Dialog
+                open={openImage2}
+                onClose={handleCloseImage2}
+                maxWidth="lg" // Adjust the maximum size of the dialog if needed
+                aria-labelledby="image-dialog-title"
+              >
+                <DialogContent
+                  onClick={handleCloseImage2}
+                  style={{ cursor: "pointer" }}
+                >
+                  {plank.image1 && (
                     <img
                       src={plank.image2}
-                      alt="Tree Image"
-                      style={{ maxWidth: "100%", height: "auto" }}
+                      alt="Plank Image Two"
+                      style={{ maxWidth: "80vh", height: "auto" }}
                     />
                   )}
-                  </td>
-                </tr>
-                <tr>
-                  <td colSpan={2}>
-                    <strong>Moisture Checks:</strong>
+                </DialogContent>
+              </Dialog>
 
-                    <div>
-                      <WaterByPlank plankId={id} />
-                    </div>
-                  </td>
-                </tr>
-              </tbody>
-            </Table>
-          </Col>
-        </Row>
-      </Container>
-    </div>
+        </Grid>
+      </Grid>
+    </PageContentContainer>
   );
 };
 
