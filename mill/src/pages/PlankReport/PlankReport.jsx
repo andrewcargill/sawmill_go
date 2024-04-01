e3944658cc9a2bad042b9f9f6c4472a75b1fe5fc
andrewcargill <110658895+andrewcargill@users.noreply.github.com>
2024-04-01 21:24:06 +0200

    contiune styling of plank report

--- mill/src/pages/PlankReport/PlankReport.jsx
+++ mill/src/pages/PlankReport/PlankReport.jsx
@@ -7,6 +7,10 @@ import CustomBox from "../../components/CustomBoxes/CustomBoxes";
 import { Alert, Box, Card, Grid, Paper } from "@mui/material";
 import CustomTypography from "../../components/Typography/CustomTypography";
 import TabReport from "./TabReport";
+import board1 from "../../media/images/board1.png";
+import board2 from "../../media/images/board2.png";
+import board3 from "../../media/images/board3.png";
+import FingerprintIcon from "@mui/icons-material/Fingerprint";
 
 const PlankReport = () => {
   const { id } = useParams();
@@ -102,171 +106,253 @@ const PlankReport = () => {
     <PageContentContainer>
       {/* Header */}
       {/* START MARKER */}
-      <Grid container pt={2} justifyContent={"flex-end"}>
-        <Grid
-          container
-          xs={6}
-          item
-          spacing={1}
-          p={5}
-          style={{
-            borderRadius: "30px 0 0 30px",
-            backgroundColor: "black",
-            color: "white",
-          }}
-        >
-          <Grid item xs={12}>
-            <CustomTypography.heading>INTRODUCTION</CustomTypography.heading>
+
+      <Grid container bgcolor={"secondary.main"}>
+        <Grid container pt={2} justifyContent={"flex-end"}>
+          <Grid
+            container
+            xs={6}
+            item
+            spacing={1}
+            p={5}
+            style={{
+              borderRadius: "30px 0 0 30px",
+              backgroundColor: "black",
+              color: "white",
+            }}
+          >
+            <Grid item xs={12}>
+              <CustomTypography.heading>INTRODUCTION</CustomTypography.heading>
+            </Grid>
           </Grid>
         </Grid>
+
+        {/* SPACER */}
+        <Grid container height={"5vh"} spacing={1} xs={6}></Grid>
+
+        {/* PRIDE OF OWNERSHIP */}
+        <Grid container spacing={1} xs={7}>
+          <CustomBox variant="primary">
+            <Grid container spacing={1}>
+              <Grid item xs={12} pb={4}>
+                <CustomTypography.heading>
+                  Pride of Ownership
+                </CustomTypography.heading>
+              </Grid>
+              <Grid item xs={12} pb={4}>
+                <CustomTypography.subheading>
+                  Transparent forestry involves understanding the origin of your
+                  wood and the circumstances surrounding its extraction from the
+                  forest. You own {species}_{id}. It is unque. In this document
+                  we will the journey.
+                </CustomTypography.subheading>
+              </Grid>
+
+              <Grid item xs={12} pb={4}>
+                <CustomTypography.paragraph>
+                  Trees are the lungs of the earth. They are the source of life
+                  and the source of wood. Sustainable and transparent forestry
+                  is the only way to ensure that we can continue to enjoy the
+                  benefits of wood products. We believe in small scale, local
+                  forestry. We believe in the power of the individual to make a
+                  difference. We believe in the power of the individual to make
+                  a difference. Selective cutting is the only way to ensure that
+                  the forest remains healthy and that the trees can continue to
+                  grow.
+                </CustomTypography.paragraph>
+              </Grid>
+              <Grid item container xs={12} pb={4}>
+                <Grid item xs={12}>
+                  <FingerprintIcon color="secondary" style={{ fontSize: 50 }} />{" "}
+                  SAWMILL GO!
+                </Grid>
+              </Grid>
+            </Grid>
+          </CustomBox>
+        </Grid>
+
+        <Grid container m={-3} justifyContent={"flex-end"}>
+          <Grid item container xs={7} bgcolor={"dark.main"} p={3}>
+            <Grid container spacing={1}>
+              <Grid
+                item
+                xs={4}
+                style={{ width: "100%", height: "200px", position: "relative" }}
+              >
+                <div
+                  style={{ width: "100%", height: "100%", overflow: "hidden" }}
+                >
+                  <img
+                    src={board1}
+                    alt="Uploaded Image"
+                    style={{
+                      height: "100%",
+                      width: "auto",
+                      objectFit: "cover",
+                      display: "block",
+                    }}
+                  />
+                </div>
+              </Grid>
+              <Grid
+                item
+                xs={4}
+                style={{ width: "100%", height: "200px", position: "relative" }}
+              >
+                <div
+                  style={{ width: "100%", height: "100%", overflow: "hidden" }}
+                >
+                  <img
+                    src={board2}
+                    alt="Uploaded Image"
+                    style={{
+                      height: "100%",
+                      width: "auto",
+                      objectFit: "cover",
+                      display: "block",
+                    }}
+                  />
+                </div>
+              </Grid>
+              <Grid
+                item
+                xs={4}
+                style={{ width: "100%", height: "200px", position: "relative" }}
+              >
+                <div
+                  style={{ width: "100%", height: "100%", overflow: "hidden" }}
+                >
+                  <img
+                    src={board3}
+                    alt="Uploaded Image"
+                    style={{
+                      height: "100%",
+                      width: "auto",
+                      objectFit: "cover",
+                      display: "block",
+                    }}
+                  />
+                </div>
+              </Grid>
+            </Grid>
+          </Grid>
+        </Grid>
+
+        {/* SPACER */}
+        <Grid container height={"5vh"} spacing={1} xs={6}></Grid>
       </Grid>
 
-      {/* SPACER */}
-      <Grid container height={"5vh"} spacing={1} xs={6}></Grid>
-
-      {/* PRIDE OF OWNERSHIP */}
-      <Grid container spacing={1} xs={7}>
-        <CustomBox variant="primary">
-          <Grid container spacing={1}>
+      <Grid container bgcolor={'primary.main'}>
+        {/* ORIGIN TREE MARKER */}
+        <Grid container pt={2} justifyContent={"flex-end"}>
+          <Grid
+            container
+            xs={6}
+            item
+            spacing={1}
+            p={5}
+            style={{
+              borderRadius: "30px 0 0 30px",
+              backgroundColor: "black",
+              color: "white",
+            }}
+          >
             <Grid item xs={12}>
               <CustomTypography.heading>
-                Pride of Ownership
+                THE ORIGIN: THE TREE
               </CustomTypography.heading>
             </Grid>
-            <Grid item xs={12}>
-              <CustomTypography.subheading>
-                Transparent forestry involves understanding the origin of your
-                wood and the circumstances surrounding its extraction from the
-                forest. You own {species}_{id}. It is unque. In this document we
-                will the journey.
-              </CustomTypography.subheading>
-            </Grid>
-
-            <Grid item xs={12}>
-              <CustomTypography.paragraph>
-                Trees are the lungs of the earth. They are the source of life
-                and the source of wood. Sustainable and transparent forestry is
-                the only way to ensure that we can continue to enjoy the
-                benefits of wood products. We believe in small scale, local
-                forestry. We believe in the power of the individual to make a
-                difference. We believe in the power of the individual to make a
-                difference. Selective cutting is the only way to ensure that the
-                forest remains healthy and that the trees can continue to grow.
-              </CustomTypography.paragraph>
-            </Grid>
-          </Grid>
-        </CustomBox>
-      </Grid>
-
-      {/* SPACER */}
-      <Grid container height={"5vh"} spacing={1} xs={6}></Grid>
-
-      {/* ORIGIN MARKER */}
-      <Grid container pt={2} justifyContent={"flex-end"}>
-        <Grid
-          container
-          xs={6}
-          item
-          spacing={1}
-          p={5}
-          style={{
-            borderRadius: "30px 0 0 30px",
-            backgroundColor: "black",
-            color: "white",
-          }}
-        >
-          <Grid item xs={12}>
-            <CustomTypography.heading>
-              THE ORIGIN: THE TREE
-            </CustomTypography.heading>
           </Grid>
         </Grid>
-      </Grid>
 
-      {/* SPACER */}
-      <Grid container height={"5vh"} spacing={1} xs={6}></Grid>
+        {/* SPACER */}
+        <Grid container height={"5vh"} spacing={1} xs={6}></Grid>
+        <Grid container justifyContent={'flex-end'} mr={6} >
+        <Grid container item spacing={1} xs={8}>
+          <CustomBox variant="white">
+            <Grid container spacing={1}>
+              <Grid item xs={12} pb={4}>
+                <CustomTypography.heading>
+                  {formatDate(treeData.date)}
+                </CustomTypography.heading>
+              </Grid>
+              <Grid item xs={12} pb={4}>
+                <CustomTypography.subheading>
+                  Transparent forestry involves understanding the origin of your
+                  wood and the circumstances surrounding its extraction from the
+                  forest.
+                </CustomTypography.subheading>
+              </Grid>
+              <Grid item xs={12} pb={4}>
+                <CustomTypography.paragraph>
+                  You own {species}_{id}. It is unque. In this document we will
+                  share what we know.
+                </CustomTypography.paragraph>
+              </Grid>
+            </Grid>
+          </CustomBox>
+        </Grid>
+        </Grid>
 
-      <Grid container spacing={1} xs={8} justifySelf={"flex-end"}>
-        <CustomBox variant="secondary">
-          <Grid container spacing={1}>
-            <Grid item xs={12}>
-              <CustomTypography.heading>
-                {formatDate(treeData.date)}
-              </CustomTypography.heading>
-            </Grid>
-            <Grid item xs={12}>
-              <CustomTypography.subheading>
-                Transparent forestry involves understanding the origin of your
-                wood and the circumstances surrounding its extraction from the
-                forest.
-              </CustomTypography.subheading>
-            </Grid>
-            <Grid item xs={12}>
-              <CustomTypography.paragraph>
-                You own {species}_{id}. It is unque. In this document we will
-                share what we know.
-              </CustomTypography.paragraph>
+        <Grid container m={-3} justifyContent={"flex-start"}>
+          <Grid
+            item
+            container
+            xs={4}
+            ml={7}
+            borderRadius={"15px"}
+            bgcolor={"secondary.main"}
+            p={3}
+          >
+            <Grid container spacing={1}>
+              <Grid
+                item
+                xs={12}
+                style={{ width: "100%", height: "400px", position: "relative" }}
+              >
+                <div
+                  style={{
+                    display: "flex",
+                    width: "100%",
+                    height: "100%",
+                    overflow: "hidden",
+                    justifyContent: "center",
+                  }}
+                >
+                  <img
+                    src={treeImage}
+                    alt="Uploaded Image"
+                    style={{
+                      height: "120%",
+                      width: "auto",
+                      objectFit: "cover",
+                      display: "block",
+                    }}
+                  />
+                </div>
+              </Grid>
             </Grid>
           </Grid>
-        </CustomBox>
+        </Grid>
+
+        {/* SPACER */}
+        <Grid container height={"5vh"} spacing={1} xs={6}></Grid>
+
+        {/*TREE IMAGE & GPS MAP */}
+
+        {/* SPACER */}
+        <Grid container height={"5vh"} spacing={1} xs={6}></Grid>
       </Grid>
-      {/*TREE IMAGE & GPS MAP */}
 
-      <Box
-        p={1}
-        sx={{
-          width: "100%",
-          display: "flex",
-          flexDirection: "column",
-          alignItems: "center",
-        }}
-      >
-        <Grid
-          xs={12}
-          style={{
-            display: "flex",
-            justifyContent: "center",
-            alignItems: "center",
-            flex: "1",
-          }}
-        >
-          {/*Select*/}
-          <CustomTypography.heading>your tree</CustomTypography.heading>
-        </Grid>
-
-        <Grid Container style={{ maxWidth: "400px", flex: "1" }}>
-          <Card>
-            <Grid
-              id="treeImage"
-              item
-              xs={6}
-              style={{
-                display: "flex",
-                justifyContent: "center",
-                alignItems: "center",
-              }}
-            >
-              {/*Tree Image */}
-              <img
-                src={treeImage}
-                alt="Uploaded Image"
-                height="300"
-                style={{}}
-              />
-            </Grid>
-          </Card>
-        </Grid>
-      </Box>
-
-      <CustomBox width={"50%"}>
+      <CustomBox>
         <Paper>
           <MyMapComponent tree={treeData} sx={{ maxWidth: "500px" }} />
         </Paper>
       </CustomBox>
-
       {/* SPACER */}
       <Grid container height={"5vh"} spacing={1} xs={6}></Grid>
 
+      <Grid container bgcolor={'secondary.main'}>
       {/* LOGGING MARKER */}
       <Grid container pt={2} justifyContent={"flex-end"}>
         <Grid
@@ -291,14 +377,14 @@ const PlankReport = () => {
 
       {/* SPACER */}
       <Grid container height={"5vh"} spacing={1} xs={6}></Grid>
-      
+
       {/* LOGGING FACTS */}
       <Grid container spacing={1} xs={7}>
         <CustomBox variant="primary">
           <Grid container spacing={1}>
             <Grid item xs={12}>
               <CustomTypography.heading>
-                LOGGING
+                {formatDate(log.date)}
               </CustomTypography.heading>
             </Grid>
             <Grid item xs={12}>
@@ -328,7 +414,7 @@ const PlankReport = () => {
 
       {/* SPACER */}
       <Grid container height={"5vh"} spacing={1} xs={6}></Grid>
-
+    </Grid>
       {/* MILLING MARKER */}
       <Grid container pt={2} justifyContent={"flex-end"}>
         <Grid
@@ -359,7 +445,7 @@ const PlankReport = () => {
           <Grid container spacing={1}>
             <Grid item xs={12}>
               <CustomTypography.heading>
-                {formatDate(treeData.date)}
+                {formatDate(plank.date)}
               </CustomTypography.heading>
             </Grid>
             <Grid item xs={12}>
@@ -379,10 +465,10 @@ const PlankReport = () => {
         </CustomBox>
       </Grid>
 
-       {/* SPACER */}
-       <Grid container height={"5vh"} spacing={1} xs={6}></Grid>
+      {/* SPACER */}
+      <Grid container height={"5vh"} spacing={1} xs={6}></Grid>
 
-       {/* END PRODUCT MARKER */}
+      {/* END PRODUCT MARKER */}
       <Grid container pt={2} justifyContent={"flex-end"}>
         <Grid
           container
@@ -397,24 +483,20 @@ const PlankReport = () => {
           }}
         >
           <Grid item xs={12}>
-            <CustomTypography.heading>
-              THE END PRODUCT
-            </CustomTypography.heading>
+            <CustomTypography.heading>THE END PRODUCT</CustomTypography.heading>
           </Grid>
         </Grid>
       </Grid>
 
-           {/* SPACER */}
-       <Grid container height={"5vh"} spacing={1} xs={6}></Grid>
+      {/* SPACER */}
+      <Grid container height={"5vh"} spacing={1} xs={6}></Grid>
 
-       {/* LOGGING FACTS */}
-       <Grid container spacing={1} xs={7}>
+      {/* LOGGING FACTS */}
+      <Grid container spacing={1} xs={7}>
         <CustomBox variant="primary">
           <Grid container spacing={1}>
             <Grid item xs={12}>
-              <CustomTypography.heading>
-                END PRODUCT
-              </CustomTypography.heading>
+              <CustomTypography.heading>END PRODUCT</CustomTypography.heading>
             </Grid>
             <Grid item xs={12}>
               <CustomTypography.subheading>
@@ -441,8 +523,8 @@ const PlankReport = () => {
         </CustomBox>
       </Grid>
 
-       {/* SPACER */}
-       <Grid container height={"5vh"} spacing={1} xs={6}></Grid>
+      {/* SPACER */}
+      <Grid container height={"5vh"} spacing={1} xs={6}></Grid>
 
       {/* Transparent Forestry */}
       <CustomBox variant="secondary" marginBottom={2}>
